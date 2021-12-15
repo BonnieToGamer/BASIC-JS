5d51d12015ca5bce3b7b5b5211a784e03006b9ca
BonnieToGamer <BonnieToGamer@lemons.se>
2021-12-15 21:11:46 +0100

    half of episode 2

--- index.js
+++ index.js
@@ -1,24 +1,20 @@
 import inq from "inquirer";
-import Lexer from "./lib/lexer.js";
-const { prompt } = inq;
-console.log('basic interpreter written in Javascript');
+import Basic from "./lib/basic.js";
 
+const { prompt } = inq;
 const question = [{
     type: 'input',
     name: 'text',
     message: 'basic>'
 }];
 
+console.log('basic interpreter written in Javascript');
+
 while (true) {
     const response = await prompt(question);
     const text = response['text'];
-    let [result, error] = run('<stdin>', text);
+    let [result, error] = Basic.run('<stdin>', text);
 
     if (error) console.error(error.toString());
-    else console.log(`[ ${result}]`);
-}
-
-function run(fn, text) {
-    const lexer = new Lexer(fn, text);
-    return lexer.makeTokens();
+    else console.log(result.toString());
 }
